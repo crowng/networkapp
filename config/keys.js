56c768d97be2de1113f88e164ab2c34b14080f0b
Gabriel Corona <coronagjk@gmail.com>
2019-06-18 10:12:11 -0700

    initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Gabriel:Corona760@networkaopp-udi4b.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// @route       GET api/Profile/test
+// @desc        Tests Profile Route
+// @access      Public
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+module.exports = router;
